72dadaa7429fb243d48ab92a07d7be7893031bd6
Thomas Koelman <thomaskoelman@hotmail.com>
2024-06-22 18:45:07 +0200

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const timeLeft = document.querySelector('#time-left')
 const score = document.querySelector('#score')
 
 let result = 0
+let molePosition
+let timer
+let currentTime = 60
 
 function randomInt(maxInt) {
     const random = Math.random()
@@ -11,25 +14,43 @@ function randomInt(maxInt) {
     return Math.floor(maxRandom)
 }
 
-function randomSquare() {
+function moveMole() {
     squares.forEach(square => {
         square.classList.remove('mole')
     })
     const index = randomInt(9)
     const randomSquare = squares[index]
     randomSquare.classList.add('mole')
+    molePosition = randomSquare.id
+
+    countDown()
 }
 
-function moveMole() {
-
+function activateMovement() {
+    timer = setInterval(moveMole, 1000)
 }
 
 function countDown() {
+    currentTime--
+    timeLeft.textContent = currentTime
 
+    if (currentTime == 0) {
+        clearInterval(timer)
+        alert('game over! score: ' + result)
+    }
 }
 
 function app() {
- randomSquare()
+ squares.forEach(square => {
+    square.addEventListener('mousedown', () => {
+        if (square.id == molePosition) {
+            result++
+            score.textContent = result
+            molePosition = null
+        }
+    })
+ })
+ activateMovement()
 }
 
 app()
