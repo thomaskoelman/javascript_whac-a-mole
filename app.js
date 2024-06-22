c27bf911abb7f1398fffed1325b795247f3236bc
Thomas Koelman <thomaskoelman@hotmail.com>
2024-06-23 00:56:26 +0200

    unused variable

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const squares = document.querySelectorAll('.square')
-const mole = document.querySelector('.mole')
+//const mole = document.querySelector('.mole')
 const timeLeft = document.querySelector('#time-left')
 const score = document.querySelector('#score')
 
